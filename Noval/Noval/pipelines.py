e63121eecbbc56f265a8ad5271eb07425109b7db
Yznx04 <2687927133@qq.com>
2018-12-12 17:58:30 +0800

    实现数据库插入

--- Noval/Noval/pipelines.py
+++ Noval/Noval/pipelines.py
@@ -4,8 +4,23 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import pymysql
 
 class NovalPipeline(object):
     def process_item(self, item, spider):
-        return item
+        name = item['book_name'][0]
+        author = item['author']
+        category = item['category']
+        word_number = item['word_number']
+        toupdate = item['toupdate']
+        status = item['status']
+        href = item['href']
+        content = item['content']
+        db = pymysql.connect(host='120.78.78.202', port=3306, user='root',
+                             passwd='420420', db='ptwx', charset='utf8')
+        cur = db.cursor()
+        sql = "INSERT INTO allbook(bookname, author, category, word_number, toupdate, status, href, content) values " \
+              "(%s, %s, %s, %s, %s, %s, %s, %s)"
+        cur.execute(sql, (name,author, category, word_number, toupdate, status,
+                          href, content))
+        db.commit()
--- Noval/Noval/text.py
+++ Noval/Noval/text.py
@@ -1,18 +1,37 @@
-import re
+# import re
+#
+# import requests
+# import bs4
+# from lxml import etree
+# url = 'https://www.piaotian.com/bookinfo/9/9910.html'
+# header = {
+#     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
+# }
+# response = requests.get(url=url, headers=header)
+# txt = response.content.decode('gbk')
+# html = etree.HTML(txt)
+# name = html.xpath('//*[@id="content"]/table/tr[4]/td/table/tr/td[2]/div/text()')
+# name = ''.join(name)
+# name = name.strip()
+# name = re.sub(r'\r', '', name)
 
-import requests
-import bs4
-from lxml import etree
-url = 'https://www.piaotian.com/bookinfo/9/9910.html'
-header = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'
-}
-response = requests.get(url=url, headers=header)
-txt = response.content.decode('gbk')
-html = etree.HTML(txt)
-name = html.xpath('//*[@id="content"]/table/tr[4]/td/table/tr/td[2]/div/text()')
-name = ''.join(name)
-name = name.strip()
-name = re.sub(r'\r', '', name)
 
+import pymysql
+book = {'author': '阴险的悟净',
+         'book_name': ['大怪医'],
+         'category': '都市言情',
+         'content': '以医入道未济世，功德十万方问仙！',
+        'href': "https://www.piaotian.com/html/6/6966/index.html",
+        'status': '连载中',
+        'toupdate': '2016-11-17',
+        'word_number': '1794394字'
+        }
+db = pymysql.connect(host='120.78.78.202', port=3306, user='root', passwd='420420', db='ptwx', charset='utf8')
+print(type(book['href']))
+print(book['content'])
+c = db.cursor()
 
+s3 = "INSERT INTO allbook(bookname, author, category, word_number, toupdate, status, href, content) values " \
+     "(%s, %s, %s, %s, %s, %s, %s, %s)"
+c.execute(s3, (book['book_name'][0], book['author'] , book['category'], book['word_number'], book['toupdate'], book['status'], book['href'], book['content'], ))
+db.commit()
